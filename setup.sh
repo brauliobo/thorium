162ab9a6c9e900a32432f746e0e46d3902ef210b
Braulio Oliveira <brauliobo@gmail.com>
2026-05-31 15:39:09 -0300

    history: drop startup delete directive backlog

--- setup.sh
+++ setup.sh
@@ -129,6 +129,7 @@ patchThor () {
 	apply_patch_once "${CR_SRC_DIR}" "${THORIUM_ROOT}/other/GPC.patch" "Global Privacy Control patch" &&
 	apply_patch_once "${CR_SRC_DIR}" "${THORIUM_ROOT}/other/disable-privacy-sandbox.patch" "Disable Privacy Sandbox patch" &&
 	apply_patch_once "${CR_SRC_DIR}" "${THORIUM_ROOT}/other/history-query-dedupe.patch" "History duplicate query patch" &&
+	apply_patch_once "${CR_SRC_DIR}" "${THORIUM_ROOT}/other/history-delete-directive-startup-guard.patch" "History delete directive startup guard patch" &&
 	apply_patch_once "${CR_SRC_DIR}" "${THORIUM_ROOT}/other/google-api-keys-defaults.patch" "Google API key defaults patch" &&
 	apply_patch_once "${CR_SRC_DIR}/v8" "${THORIUM_ROOT}/other/v8-simd-buildflags.patch" "V8 SIMD build flags patch" &&
 	# Kept because it is related to Thorium custom flags (not upstreamable).
